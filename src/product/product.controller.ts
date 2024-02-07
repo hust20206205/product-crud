f18f4712dfdfb39ac39930cf32a2d250fedab8ba
Your New Name <nghiavu2k2abc@gmail.com>
2024-02-07 09:50:24 +0700

    update product.controller.ts

--- src/product/product.controller.ts
+++ src/product/product.controller.ts
@@ -2,10 +2,11 @@ import {
   Controller,
   Get,
   Post,
-  Body,
   Patch,
-  Param,
+  Put,
   Delete,
+  Param,
+  Body,
 } from '@nestjs/common';
 
 import { Product } from './entities/product.entity';
@@ -15,17 +16,29 @@ import { CreateProductDto } from './dto/create-product.dto';
 import { UpdateProductDto } from './dto/update-product.dto';
 import { ResponseProductDto } from './dto/response-product.dto';
 
-import { ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
+import { ApiBody, ApiOperation, ApiParam, ApiResponse, ApiTags } from '@nestjs/swagger';
 
 @Controller('product')
 @ApiTags('product')
 export class ProductController {
   constructor(private readonly productService: ProductService) {}
 
+ 
+
+
+  
   @Post()
-  create(@Body() createProductDto: CreateProductDto) {
-    return this.productService.create(createProductDto);
+  @ApiOperation({ summary: 'Create a new Product' })
+  @ApiResponse({ status: 201, type: ResponseProductDto })
+  @ApiBody({ type: CreateProductDto })
+  async create(
+    @Body() createProductDto: CreateProductDto,
+  ): Promise<Product> {
+    return await this.productService.create(createProductDto);
   }
+
+
+
 
   @Get()
   @ApiOperation({ summary: 'Get all Products' })
@@ -41,10 +54,27 @@ export class ProductController {
   async findOne(@Param('id') id: string): Promise<Product> {
     return await this.productService.findOne(id);
   }
-  @Patch(':id')
-  update(@Param('id') id: string, @Body() updateProductDto: UpdateProductDto) {
-    return this.productService.update(id, updateProductDto);
+  
+
+
+
+
+  @Put(':id')
+  @ApiOperation({ summary: 'Update a Product by id' })
+  @ApiResponse({ status: 200, type: ResponseProductDto })
+  @ApiParam({ name: 'id', required: true })
+  @ApiBody({ type: UpdateProductDto })
+  async update(
+    @Param('id') id: string,
+    @Body() updateProductDto: UpdateProductDto,
+  ): Promise<Product> {
+    return await this.productService.update(id, updateProductDto);
   }
+
+
+
+
+
 
   @Delete(':id')
   @ApiOperation({ summary: 'Delete a Product by id' })
@@ -54,42 +84,3 @@ export class ProductController {
     return await this.productService.remove(id);
   }
 }
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-// 
-//   @Post()
-//   @ApiOperation({ summary: 'Create a new Product' })
-//   @ApiResponse({ status: 201, type: ResponseProductDto })
-//   @ApiBody({ type: CreateProductDto })
-//   async create(
-//     @Body() createProductDto: CreateProductDto,
-//   ): Promise<Product> {
-//     return await this.productService.create(createProductDto);
-//   }
-
-//   @Put(':id')
-//   @ApiOperation({ summary: 'Update a Product by id' })
-//   @ApiResponse({ status: 200, type: ResponseProductDto })
-//   @ApiParam({ name: 'id', required: true })
-//   @ApiBody({ type: UpdateProductDto })
-//   async update(
-//     @Param('id') id: string,
-//     @Body() updateProductDto: UpdateProductDto,
-//   ): Promise<Product> {
-//     return await this.productService.update(id, updateProductDto);
-//   }
-// }
