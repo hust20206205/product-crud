ee0b953d90911b183ee19828b3bd451fa425ca4b
vvn20206205 <nghiavu2k2abc@gmail.com>
2024-02-12 05:26:45 +0700

    update 9 files

--- src/main.ts
+++ src/main.ts
@@ -2,10 +2,8 @@ import { NestFactory } from '@nestjs/core';
 import { ValidationPipe, Logger } from '@nestjs/common';
 import { AppModule } from './app.module';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
-import * as dotenv from 'dotenv';
 
 async function bootstrap() {
-  dotenv.config();
   const app = await NestFactory.create(AppModule);
   const logger = new Logger();
 
@@ -14,20 +12,6 @@ async function bootstrap() {
   const options = new DocumentBuilder().build();
   const document = SwaggerModule.createDocument(app, options);
   SwaggerModule.setup('swagger', app, document);
-  // SwaggerModule.setup('swagger', app, document, {
-  //   customfavIcon: 'https://api.houstongarden.click/docs/favicon-32x32.png',
-  //   customJs: [
-  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-bundle.min.js',
-  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.js',
-  //   ],
-  //   customCssUrl: [
-  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.min.css',
-  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui-standalone-preset.min.css',
-  //     'https://cdnjs.cloudflare.com/ajax/libs/swagger-ui/4.15.5/swagger-ui.css',
-  //   ],
-  // });
-
-  //
 
   app.useGlobalPipes(
     new ValidationPipe({
